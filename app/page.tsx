d26d5c39ff7d7de5bf218bdc5a8d98dbe3728221
sarrahman-me <sarrahman.me@gmail.com>
2024-02-04 16:50:37 +0700

    :lipstick: membuat tampilan home untuk gemini pro

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,59 @@
-import Image from "next/image";
+import { TbMessageChatbot } from "react-icons/tb";
+import { RiAiGenerate } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="bg-gradient-to-tl from-sky-950 via-blue-950 to-indigo-950 h-screen">
+      <div className="text-center py-10 pt-20">
+        <h1 className="font-bold text-4xl md:text-8xl bg-gradient-to-r from-indigo-400 via-cyan-400 to-amber-400 inline-block text-transparent bg-clip-text">
+          Gemini Pro
+        </h1>
+        <p className="text-white md:text-base text-sm">
+          The best performing model with features for a wide variety of text and
+          image reasoning tasks.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mx-5 md:mx-10 my-10 md:my-20">
+        <CardFeature
+          title={"Chat"}
+          icon={<TbMessageChatbot />}
+          description={
+            "Gemini Chat memungkinkan Anda membangun pengalaman obrolan interaktif dengan mudah. Dengan kecerdasan tinggi dari model Gemini Pro, pengguna dapat menikmati obrolan yang responsif dan kontekstual untuk berbagai keperluan."
+          }
+        />
+        <CardFeature
+          title={"Generated Text"}
+          icon={<RiAiGenerate />}
+          description={
+            "Generated Text dari Gemini Pro memberikan kemampuan untuk menghasilkan teks berkualitas tinggi dengan berbagai konten. Mulai dari pembuatan artikel, paragraf inspiratif, hingga konten-konten kreatif, Gemini Pro dapat menghasilkan teks yang sesuai dengan kebutuhan Anda."
+          }
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className="text-white text-xs text-center mt-10">
+        Created By Sarrahman
+      </p>
     </main>
   );
 }
+
+const CardFeature = ({
+  title,
+  icon,
+  description,
+}: {
+  title: string;
+  icon: any;
+  description: string;
+}) => {
+  return (
+    <div className="text-white border space-y-3 border-white bg-gradient-to-br from-sky-900 via-blue-900 to-indigo-900 w-full rounded-md p-2 hover:from-sky-800 hover:via-blue-800 hover:to-indigo-800 transition ease-linear cursor-pointer">
+      <div className="flex items-center space-x-2">
+        <div className="text-2xl">{icon}</div>
+        <h3 className="text-sm md:text-base">{title}</h3>
+      </div>
+      <p className="text-xs md:text-sm">{description}</p>
+    </div>
+  );
+};
